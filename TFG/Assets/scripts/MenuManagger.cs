b22cc2967f0afe53d29bcc40263e738fdfe475b4
agaspilrre <agaspilrre@gmail.com>
2017-03-23 19:20:05 +0100

    Menu provisional

---
+++ TFG/Assets/scripts/MenuManagger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuManagger : MonoBehaviour {
+
+    //funcion para cargar la escena de juego
+    public void OnNewGameClick()
+    {
+        SceneManager.LoadScene("scenne_1");
+    }
+
+
+    //funcion para salir del juego
+    public void OnQuitClick()
+    {
+        Application.Quit();//sale del juego
+    }
+
+
+}
