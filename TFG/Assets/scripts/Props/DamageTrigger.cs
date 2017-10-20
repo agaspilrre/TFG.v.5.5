ec681e3828f25dbd8739d51f70296527a89277de
agaspilrre <agaspilrre@gmail.com>
2017-10-20 15:32:02 +0200

    trigger de daño

---
+++ TFG/Assets/scripts/Props/DamageTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //si este trigger detecta al personaje le hace daño
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.GetComponent<lifeScript>().makeDamage(1);
+
+        }
+
+    }
+}
