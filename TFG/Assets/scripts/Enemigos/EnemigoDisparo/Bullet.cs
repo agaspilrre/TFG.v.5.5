61a3353ae736f19c83085e87840ec24546989313
agaspilrre <agaspilrre@gmail.com>
2017-11-07 10:43:21 +0100

    Enemigo Disparo Triple

---
+++ TFG/Assets/scripts/Enemigos/EnemigoDisparo/Bullet.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bullet : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float speed;
+
+    //GameObject player;
+    public bool verticalDirection;
+    public bool rigthDiagonalDirection;
+    public bool leftDiagonalDirection;
+
+    public GameObject enemy;
+
+    
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //know direction bullet
+        //Enemy look up
+        if(enemy.transform.rotation.eulerAngles.z>=0 && enemy.transform.rotation.eulerAngles.z < 89)
+        {
+            //direction bullet
+            if (verticalDirection)
+                rb.velocity = new Vector2(0, 1 * speed);
+
+            else if (rigthDiagonalDirection)
+                rb.velocity = new Vector2(1 * speed, 1 * speed);
+
+            else if (leftDiagonalDirection)
+                rb.velocity = new Vector2(-1 * speed, 1 * speed);
+        }
+
+        //enemy look left
+        else if(enemy.transform.rotation.eulerAngles.z > 89 && enemy.transform.rotation.eulerAngles.z < 170)
+        {
+            if (verticalDirection)
+                rb.velocity = new Vector2(-1 * speed, 0);
+
+            else if (rigthDiagonalDirection)
+                rb.velocity = new Vector2(-1 * speed, 1 * speed);
+
+            else if (leftDiagonalDirection)
+                rb.velocity = new Vector2(-1 * speed, -1 * speed);
+        }
+
+        //enemy look down
+        else if (enemy.transform.rotation.eulerAngles.z > 170 && enemy.transform.rotation.eulerAngles.z < 260)
+        {
+            if (verticalDirection)
+                rb.velocity = new Vector2(0, -1 * speed);
+
+            else if (rigthDiagonalDirection)
+                rb.velocity = new Vector2(-1 * speed, -1 * speed);
+
+            else if (leftDiagonalDirection)
+                rb.velocity = new Vector2(1 * speed, -1 * speed);
+        }
+
+        //enemy look rigth
+        else if (enemy.transform.rotation.eulerAngles.z > 260 && enemy.transform.rotation.eulerAngles.z < 360)
+        {
+            if (verticalDirection)
+                rb.velocity = new Vector2(1 * speed, 0);
+
+            else if (rigthDiagonalDirection)
+                rb.velocity = new Vector2(1 * speed, -1 * speed);
+
+            else if (leftDiagonalDirection)
+                rb.velocity = new Vector2(1 * speed, 1 * speed);
+        }
+
+
+
+    }
+
+
+    public void setEnemy(GameObject _enemy)
+    {
+        enemy = _enemy;
+    }
+    
+    public void setSpeedBullet(float _speed)
+    {
+        speed = _speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag != "bullet" && coll.gameObject.tag != "Enemy")
+        {
+            if (coll.gameObject.tag == "Player")
+            {
+                //call die function
+               
+                coll.gameObject.GetComponent<lifeScript>().makeDamage(1);
+            }
+
+            Destroy(gameObject);
+
+        }
+         
+
+
+    }
+}
---
+++ TFG/Assets/scripts/Enemigos/EnemigoDisparo/EnemyShoot.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour {
+
+    public bool haveMovement;
+    public float rangeMovement;
+    public Transform shootVerticalPoint;
+    public Transform shootDiagonalRPoint;
+    public Transform shootDiagonalLPoint;
+
+    public GameObject verticalBullet;
+    public GameObject diagonalRBullet;
+    public GameObject diagonalLBullet;
+    GameObject bullet1;
+    GameObject bullet2;
+    GameObject bullet3;
+
+    public float speedMove;
+    public float speedBullet;
+    float directionMove = 1;
+    public int timeBetwenShoot;
+    int countTimer=0;
+    Rigidbody2D rbEnemy;
+    float initialPositionX;
+    float initialPositionY;
+
+  
+
+
+	// Use this for initialization
+	void Start () {
+
+        rbEnemy=GetComponent<Rigidbody2D>();
+        initialPositionX = this.transform.position.x;
+        initialPositionY = this.transform.position.y;
+
+        verticalBullet.GetComponent<Bullet>().setEnemy(this.gameObject);
+        diagonalLBullet.GetComponent<Bullet>().setEnemy(this.gameObject);
+        diagonalRBullet.GetComponent<Bullet>().setEnemy(this.gameObject);
+
+        verticalBullet.GetComponent<Bullet>().setSpeedBullet(speedBullet);
+        diagonalLBullet.GetComponent<Bullet>().setSpeedBullet(speedBullet);
+        diagonalRBullet.GetComponent<Bullet>().setSpeedBullet(speedBullet);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+        if (haveMovement)
+        {
+            //look rotation object enemy for make movement
+            //horizontal move
+            if((this.transform.eulerAngles.z>=0 && this.transform.eulerAngles.z<=89)||
+               ( this.transform.eulerAngles.z > 170 && this.transform.eulerAngles.z <= 260) )
+            {
+               
+                if(this.transform.position.x>=initialPositionX+rangeMovement ||
+                    this.transform.position.x <= initialPositionX- rangeMovement)
+                {
+                    directionMove *= -1;
+                }
+                rbEnemy.velocity = new Vector2(directionMove * speedMove, 0);
+
+            }
+
+            //vertical move
+            else if ((this.transform.eulerAngles.z >= 89 && this.transform.eulerAngles.z <= 169) ||
+              (this.transform.eulerAngles.z > 260 && this.transform.eulerAngles.z <= 360))
+            {
+
+                if (this.transform.position.y >= initialPositionY + rangeMovement ||
+                    this.transform.position.y <= initialPositionY - rangeMovement)
+                {
+                    directionMove *= -1;
+                }
+                rbEnemy.velocity = new Vector2(0, directionMove * speedMove);
+
+            }
+
+
+
+
+        }
+
+        //timer instantiate bullets
+        countTimer++;
+        if (countTimer > timeBetwenShoot)
+        {
+            InstantiateBullets();
+            countTimer = 0;
+        }
+
+
+		
+	}
+
+    //function to instantiate bullets
+    public void InstantiateBullets()
+    {
+        bullet1 = (GameObject)Instantiate(verticalBullet, shootVerticalPoint.position, shootVerticalPoint.rotation);
+        bullet2 = (GameObject)Instantiate(diagonalLBullet, shootDiagonalLPoint.position, shootDiagonalLPoint.rotation);
+        bullet3 = (GameObject)Instantiate(diagonalRBullet, shootDiagonalRPoint.position, shootDiagonalRPoint.rotation);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //pendiente de implementar
+        /*
+        if (coll.gameObject.tag == "NonStatic")
+        {
+            Destroy(gameObject);
+        }*/
+
+    }
+}
