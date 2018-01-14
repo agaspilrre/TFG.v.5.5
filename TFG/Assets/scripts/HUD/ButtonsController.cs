1265361d89a7b8f04f0e4f371ecf63e61ebd930a
Alberto Quesada <alberto5azul@hotmail.com>
2018-01-14 15:46:36 +0100

    Implementacion menu (no esta terminado)

---
+++ TFG/Assets/scripts/HUD/ButtonsController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonsController : MonoBehaviour {
+
+    RectTransform rectTransform;
+    public Vector3 buttonSizeOnSelect;
+
+    public GameObject blurImage;
+    public GameObject auxImage;
+    public GameObject newGameMenu;
+    public GameObject quitGameMenu;
+    public GameObject settingsMenu;
+
+    public EventSystem eventSyst;
+
+    public GameObject yesButton;
+    public GameObject startButton;
+
+	// Use this for initialization
+	void Start () {
+
+        rectTransform = GetComponent<RectTransform>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void makeBigger()
+    {
+        rectTransform.localScale = buttonSizeOnSelect;
+    }
+
+    public void reduceNormal()
+    {
+        rectTransform.localScale = new Vector3(1, 1, 1);
+    }
+
+    public void showNewGameMenu()
+    {
+        blurImage.SetActive(true);
+        auxImage.SetActive(true);
+        newGameMenu.SetActive(true);
+        eventSyst.firstSelectedGameObject = yesButton;
+    }
+
+    public void loadGame()
+    {
+
+    }
+
+    public void showOptions()
+    {
+        blurImage.SetActive(true);
+        auxImage.SetActive(true);
+        settingsMenu.SetActive(true);
+    }
+
+    public void showQuitGameMenu()
+    {
+        blurImage.SetActive(true);
+        auxImage.SetActive(true);
+        quitGameMenu.SetActive(true);
+        eventSyst.firstSelectedGameObject = yesButton;
+    }
+
+    public void yesNewGameButton()
+    {
+        SceneManager.LoadScene("FreshStart");
+    }
+
+    public void noNewGameButton()
+    {
+        blurImage.SetActive(false);
+        auxImage.SetActive(false);
+        newGameMenu.SetActive(false);
+        eventSyst.firstSelectedGameObject = startButton;
+    }
+
+    public void yesQuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void noQuitButton()
+    {
+        blurImage.SetActive(false);
+        auxImage.SetActive(false);
+        newGameMenu.SetActive(false);
+        eventSyst.firstSelectedGameObject = startButton;
+    }
+}
---
+++ TFG/Assets/scripts/HUD/MenuManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+   
+    public float fadeTime = 0.025f;
+    public RawImage fadeBlack;
+    public GameObject staticImage;
+    public RawImage[] concepts;
+
+    bool round;
+
+    // Use this for initialization
+    void Start () {
+
+        round = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Sequence();       
+    }
+
+    void FadeOut(RawImage image)
+    {        
+        Color color = image.color;
+        color.a -= fadeTime;
+        image.color = color;       
+    }
+
+    void ResetAlpha(RawImage image)
+    {
+        Color color = image.color;
+        color.a = 255;
+        image.color = color;
+                
+    }
+
+    void Sequence()
+    {
+        if (fadeBlack.color.a >= 0)
+            FadeOut(fadeBlack);
+
+        if (fadeBlack.color.a <= 0)
+            FadeOut(concepts[0]);
+
+        //FADE OUT
+
+        if (concepts[0].color.a <= 0)
+        {
+            staticImage.SetActive(true);
+            FadeOut(concepts[1]);
+        }
+
+        if (concepts[1].color.a <= 0)
+            FadeOut(concepts[2]);
+
+        if (concepts[2].color.a <= 0)
+            FadeOut(concepts[3]);
+
+        if (concepts[3].color.a <= 0)
+            FadeOut(concepts[4]);
+
+        if (concepts[4].color.a <= 0)
+        {
+            Reset();
+        }
+    }
+
+  
+
+    void Reset()
+    {
+        for(int i = 0; i < concepts.Length; i++)
+        {
+            ResetAlpha(concepts[i]);
+        }
+        
+        staticImage.SetActive(false);
+
+        
+    }
+}
